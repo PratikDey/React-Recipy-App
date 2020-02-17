d8fa7d8f8c0aebac1ef5226f580aa62cc600953b
Pratik Dey <56970651+PratikDey@users.noreply.github.com>
2020-02-17 23:49:46 +0530

    Add files via upload

---
+++ build/precache-manifest.a0037cf7f76a9d0c416f330b19a13705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2bb18b7e83bddaf17f5fe44ff5fa811",
+    "url": "/React-Recipy-App/index.html"
+  },
+  {
+    "revision": "7c9f376facc66c4efd51",
+    "url": "/React-Recipy-App/static/css/main.5204a4df.chunk.css"
+  },
+  {
+    "revision": "9d2a09ab0a8f9782e6aa",
+    "url": "/React-Recipy-App/static/js/2.7ef72192.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Recipy-App/static/js/2.7ef72192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9f376facc66c4efd51",
+    "url": "/React-Recipy-App/static/js/main.434016d3.chunk.js"
+  },
+  {
+    "revision": "29fc3c829832d274c142",
+    "url": "/React-Recipy-App/static/js/runtime-main.423390a8.js"
+  }
+]);
